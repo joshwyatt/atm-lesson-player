cb7ca98504447850f3d597c0a88bb454495bf7ec
joshWyatt <joshuawyatt123@gmail.com>
2014-10-15 19:27:42 -0700

    Add gulp scripts task

--- gulpFile.js
+++ gulpFile.js
@@ -1,15 +1,31 @@
 ;(function(){
   'use strict';
 
+  // required modules
   var gulp = require('gulp');
   var $ = require('gulp-load-plugins')({lazy: false});
   var server = require('./server/server.js');
+  var paths = require('./gulp_helpers/paths.js');
+  var options = require('./gulp_helpers/options.js');
 
+  // tasks
   gulp.task('default', $.sequence('serve'));
+
+  gulp.task('scripts', scripts);
   gulp.task('serve', serve);
 
+  // task functions
   function serve(){
     server();
   }
 
+  function scripts(){
+    return gulp.src(paths.scripts)
+      .pipe($.jshint())
+      .pipe($.concat('app.min.js'))
+      .pipe($.uglify())
+      .pipe(gulp.dest(paths.dist))
+      .pipe($.livereload());
+  }
 })();
+
---
+++ gulp_helpers/paths.js
@@ -0,0 +1,12 @@
+;(function(){
+  'use strict';
+
+  var paths = {};
+
+  paths.scripts = './client/www/**/*.js';
+  paths.dist = './dist';
+  
+
+  module.exports = paths;
+
+})();
